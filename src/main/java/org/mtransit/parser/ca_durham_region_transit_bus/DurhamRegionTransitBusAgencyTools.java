0b276b6f9b2629d240b16fc62566509be0824a24
Mathieu Méa <mathieu.mea@gmail.com>
2022-07-20 12:34:38 -0400

    Merge complex direction headsign "X abc" & "Y def" -> "abc / def"

--- src/main/java/org/mtransit/parser/ca_durham_region_transit_bus/DurhamRegionTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_durham_region_transit_bus/DurhamRegionTransitBusAgencyTools.java
@@ -10,9 +10,9 @@ import org.mtransit.commons.CleanUtils;
 import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
+import org.mtransit.parser.mt.data.MTrip;
 
 import java.util.Locale;
 import java.util.regex.Pattern;
@@ -54,202 +54,14 @@ public class DurhamRegionTransitBusAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
-	@NotNull
 	@Override
-	public String getRouteLongName(@NotNull GRoute gRoute) {
-		if (StringUtils.isEmpty(gRoute.getRouteLongName())) {
-			if ("101A".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Industrial Bay Ridges";
-			} else if ("219C".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Ravenscroft";
-			} else if ("232".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Church";
-			} else if ("308C".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Whitby Shores";
-			} else if ("310".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Brooklin / UOIT & DC";
-			} else if ("401C".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Simcoe";
-			} else if ("417".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Conlin";
-			} else if ("506".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Wilmot Crk Orono Newcastle"; // Community Bus
-			} else if ("910C".equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return "Campus Connect";
-			}
-			throw new MTLog.Fatal("Unexpected route long name for %s!", gRoute);
-		}
-		return super.getRouteLongName(gRoute);
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
 	}
 
 	@Override
 	public boolean defaultAgencyColorEnabled() {
 		return true;
-	}
-
-	@Nullable
-	@Override
-	public String getRouteColor(@NotNull GRoute gRoute, @NotNull MAgency agency) {
-		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
-			final String rsnS = gRoute.getRouteShortName();
-			if (CharUtils.isDigitsOnly(rsnS)) {
-				final int rsn = Integer.parseInt(rsnS);
-				switch (rsn) {
-				// @formatter:off
-				case 101: return "6345A6";
-				case 103: return "F2C61D";
-				case 107: return "4866BA";
-				case 110: return "ED1C24";
-				case 111: return "2C3792";
-				case 112: return "2F684C";
-				case 120: return "CC8ACB";
-				case 181: return "A9A9A9"; // ???
-				case 182: return "A9A9A9"; // ???
-				case 193: return "00ADEF";
-				case 199: return "AC74C1";
-				case 215: return "9587B7";
-				case 216: return "754C29";
-				case 217: return "0075BF";
-				case 218: return "988FD0";
-				case 219: return "4D71C1";
-				case 221: return "F3B722";
-				case 222: return "54C6BD";
-				case 223: return "602C83";
-				case 224: return "64C430";
-				case 225: return "9A8D7D";
-				case 226: return "6345A6";
-				case 232: return "EE2428";
-				case 283: return "A9A9A9"; // ???
-				case 284: return "A9A9A9"; // ???
-				case 291: return "33B652";
-				case 301: return "8D3CA3";
-				case 302: return "EE2428";
-				case 303: return "6EC95F";
-				case 304: return "79D0ED";
-				case 305: return "8448a9";
-				case 308: return "AC57B2";
-				case 310: return "DFC463";
-				case 312: return "AAA8A9";
-				case 318: return "9BA821";
-				case 385: return "A9A9A9"; // ???
-				case 401: return "EE2428";
-				case 402: return "6E2932";
-				case 403: return "4E5BB3";
-				case 405: return "C6A218";
-				case 406: return "00ADEF";
-				case 407: return "AAA8A9";
-				case 408: return "ED1C24";
-				case 409: return "4159B1";
-				case 410: return "A14A94";
-				case 411: return "536DBE";
-				case 412: return "85D168";
-				case 414: return "CE92CF";
-				case 416: return "EC5788";
-				case 417: return "74459B";
-				case 420: return "324954";
-				case 422: return "EA6C14";
-				case 501: return "2D4CA3";
-				case 502: return "5FC3F0";
-				case 506: return "F14823";
-				case 601: return "2D4CA3";
-				case 603: return "F14623";
-				case 653: return "8D188F";
-				case 654: return "293D9B";
-				case 750: return null; // TODO
-				case 752: return null; // TODO
-				case 801: return "4EA3DD";
-				case 900: return "DE581E"; // PULSE
-				case 901: return "DE581E"; // PULSE
-				case 902: return null; // TODO
-				case 905: return null; // TODO
-				case 910: return "75AC95";
-				case 915: return "36612B";
-				case 916: return "5D1B1E";
-				case 917: return null; // TODO
-				case 922: return "231F20";
-				case 923: return "65A930";
-				case 950: return "87CF25";
-				case 960: return "B2D235";
-				case 980: return null; // TODO
-				// @formatter:on
-				}
-			}
-			if ("101A".equalsIgnoreCase(rsnS)) {
-				return "6345A6"; // 101
-			} else if ("103B".equalsIgnoreCase(rsnS)) {
-				return "A7516D";
-			} else if ("110A".equalsIgnoreCase(rsnS)) {
-				return "ED1C24"; // 110
-			} else if ("110B".equalsIgnoreCase(rsnS)) {
-				return "ED1C24"; // 110
-			} else if ("110sh".equalsIgnoreCase(rsnS)) {
-				return "ED1C24"; // 110
-			} else if ("111A".equalsIgnoreCase(rsnS)) {
-				return "2C3792"; // 111
-			} else if ("111S".equalsIgnoreCase(rsnS)) {
-				return "2C3792"; // 111
-			} else if ("112CS".equalsIgnoreCase(rsnS)) {
-				return "2F684C"; // 112
-			} else if ("112SH".equalsIgnoreCase(rsnS)) {
-				return "2F684C"; // 112
-			} else if ("193A".equalsIgnoreCase(rsnS)) {
-				return "00ADEF"; // 193
-			} else if ("193B".equalsIgnoreCase(rsnS)) {
-				return "00ADEF"; // 193
-			} else if ("218D".equalsIgnoreCase(rsnS)) {
-				return "988FD0"; // 218
-			} else if ("219C".equalsIgnoreCase(rsnS)) {
-				return "4D71C1"; // 219
-			} else if ("219D".equalsIgnoreCase(rsnS)) {
-				return "4D71C1"; // 219
-			} else if ("219DSH".equalsIgnoreCase(rsnS)) {
-				return "4D71C1"; // 219
-			} else if ("221D".equalsIgnoreCase(rsnS)) {
-				return "F3B722"; // 221
-			} else if ("223C".equalsIgnoreCase(rsnS)) {
-				return "602C83"; // 223
-			} else if ("224D".equalsIgnoreCase(rsnS)) {
-				return "64C430"; // 224
-			} else if ("224DSH".equalsIgnoreCase(rsnS)) {
-				return "64C430"; // 224 //
-			} else if ("225A".equalsIgnoreCase(rsnS)) {
-				return "9A8D7D"; // 225
-			} else if ("226S".equalsIgnoreCase(rsnS)) {
-				return "6345A6"; // 226
-			} else if ("232S".equalsIgnoreCase(rsnS)) {
-				return "EE2428"; // 232
-			} else if ("308C".equalsIgnoreCase(rsnS)) {
-				return "AC57B2"; // 308
-			} else if ("318SH".equalsIgnoreCase(rsnS)) {
-				return "9BA821"; // 318
-			} else if ("401B".equalsIgnoreCase(rsnS)) {
-				return "8AD9F2";
-			} else if ("401C".equalsIgnoreCase(rsnS)) {
-				return "6C57B1";
-			} else if ("402B".equalsIgnoreCase(rsnS)) {
-				return "9BA821";
-			} else if ("407S".equalsIgnoreCase(rsnS)) {
-				return "AAA8A9"; // 407
-			} else if ("407C".equalsIgnoreCase(rsnS)) {
-				return "AAA8A9"; // 407
-			} else if ("407CS".equalsIgnoreCase(rsnS)) {
-				return "AAA8A9"; // 407
-			} else if ("410S".equalsIgnoreCase(rsnS)) {
-				return "A14A94"; // 410
-			} else if ("411S".equalsIgnoreCase(rsnS)) {
-				return "536DBE"; // 411
-			} else if ("412S".equalsIgnoreCase(rsnS)) {
-				return "85D168"; // 412
-			} else if ("412CS".equalsIgnoreCase(rsnS)) {
-				return "85D168"; // 412
-			} else if ("910C".equalsIgnoreCase(rsnS)) {
-				return "9BA821";
-			} else if ("922B".equalsIgnoreCase(rsnS)) {
-				return "3DA87F";
-			}
-			throw new MTLog.Fatal("Unexpected route color %s!", gRoute.toStringPlus());
-		}
-		return super.getRouteColor(gRoute, agency);
 	}
 
 	@NotNull
@@ -262,6 +74,46 @@ public class DurhamRegionTransitBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public boolean directionFinderEnabled() {
 		return true;
+	}
+
+	private static final Pattern STARTS_WITH_LETTER = Pattern.compile("(^[a-z] )", Pattern.CASE_INSENSITIVE);
+
+	@Nullable
+	@Override
+	public String selectDirectionHeadSign(@Nullable String headSign1, @Nullable String headSign2) {
+		if (StringUtils.equals(headSign1, headSign2)) {
+			return null; // can NOT select
+		}
+		final boolean startsWithLetter1 = headSign1 != null && STARTS_WITH_LETTER.matcher(headSign1).find();
+		final boolean startsWithLetter2 = headSign2 != null && STARTS_WITH_LETTER.matcher(headSign2).find();
+		if (startsWithLetter1) {
+			if (!startsWithLetter2) {
+				return headSign2;
+			}
+		} else if (startsWithLetter2) {
+			return headSign1;
+		}
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String mergeComplexDirectionHeadSign(@Nullable String headSign1, @Nullable String headSign2) {
+		if (StringUtils.equals(headSign1, headSign2)) {
+			return null; // can NOT select
+		}
+		if (headSign1 == null || headSign2 == null) {
+			return null;
+		}
+		final boolean startsWithLetter1 = STARTS_WITH_LETTER.matcher(headSign1).find();
+		final boolean startsWithLetter2 = STARTS_WITH_LETTER.matcher(headSign2).find();
+		if (startsWithLetter1 && startsWithLetter2) {
+			return MTrip.mergeHeadsignValue(
+					headSign1.substring(2),
+					headSign2.substring(2)
+			);
+		}
+		return null;
 	}
 
 	private static final Pattern DASH_ = Pattern.compile("( - | -|- )", Pattern.CASE_INSENSITIVE);
